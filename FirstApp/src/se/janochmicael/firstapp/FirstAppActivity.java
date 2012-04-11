544f404743f88dee9f1c70425ae757937d8ea3f1
micael <micael.vesterlund@gmail.com>
2012-04-11 12:30:10 +0200

    json

--- FirstApp/src/se/janochmicael/firstapp/FirstAppActivity.java
+++ FirstApp/src/se/janochmicael/firstapp/FirstAppActivity.java
@@ -3,41 +3,57 @@ package se.janochmicael.firstapp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class FirstAppActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.main);
-        TextView tv = (TextView) findViewById(R.id.hello);
-        
-        URL oracle;
-        BufferedReader in;
-        String inputLine = "";
-        String response = "";
+		setContentView(R.layout.main);
+		TextView calendarTV = (TextView) findViewById(R.id.calendar);
+
+		String calendarText = "";
+		String response = getCalendarString();
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			String date = jsonObject.getString("datum");
+			calendarText = date;
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		calendarTV.setText(calendarText);
+
+	}
+
+	private String getCalendarString() {
+		URL oracle;
+		BufferedReader in;
+		String inputLine = "";
+		String response = "";
 		try {
 			oracle = new URL("http://api.dryg.net/dagar/v1/");
 			in = new BufferedReader(new InputStreamReader(oracle.openStream()));
 			while ((inputLine = in.readLine()) != null) {
-			    System.out.println(inputLine);
-			    response += inputLine;
+				response += inputLine;
 			}
 
-	        in.close();
+			in.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        tv.setText(response);
-        
-    }
+		return response;
+	}
 }
