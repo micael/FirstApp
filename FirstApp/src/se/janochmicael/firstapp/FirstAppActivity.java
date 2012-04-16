20e842c2cfc1788a953ab4e60fb5db77fdc3c02f
luyang <janne_lund@yahoo.com>
2012-04-16 17:50:07 +0200

    date picker, submit button & helgdag

--- FirstApp/src/se/janochmicael/firstapp/FirstAppActivity.java
+++ FirstApp/src/se/janochmicael/firstapp/FirstAppActivity.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.json.JSONException;
@@ -12,18 +13,45 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class FirstAppActivity extends Activity {
+	Button submitButton;
+	DatePicker datePicker;
+	TextView calendarTV;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main);
-		TextView calendarTV = (TextView) findViewById(R.id.calendar);
 
-		String response = getCalendarString();
+		submitButton = (Button) findViewById(R.id.submitButton);
+		datePicker = (DatePicker) findViewById(R.id.datePicker1);
+
+		submitButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				Date d = new Date(datePicker.getYear() - 1900, datePicker
+						.getMonth(), datePicker.getDayOfMonth());
+				updateText(getCalendarString(d));
+			}
+
+		});
+		// submitButton.on
+
+		calendarTV = (TextView) findViewById(R.id.calendar);
+
+		String response = getCalendarString(Calendar.getInstance().getTime());
+		updateText(response);
+
+	}
+
+	private void updateText(String response) {
 		try {
 			JSONObject jsonObject = new JSONObject(response);
 			StringBuffer calendarText = parseAndFormat(jsonObject);
@@ -32,10 +60,10 @@ public class FirstAppActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
-	private StringBuffer parseAndFormat(JSONObject jsonObject) throws JSONException {
+	private StringBuffer parseAndFormat(JSONObject jsonObject)
+			throws JSONException {
 		StringBuffer calendarText = new StringBuffer();
 		String datum = jsonObject.getString("datum");
 		String unixdatum = jsonObject.getString("unixdatum");
@@ -54,19 +82,33 @@ public class FirstAppActivity extends Activity {
 		// calendarText.append(veckodag);
 		calendarText.append(" V:");
 		calendarText.append(vecka);
+		try {
+			String helgdag = jsonObject.getString("helgdag");
+			if (helgdag != null && !"".equals(helgdag)) {
+				calendarText.append(" ").append(helgdag);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return calendarText;
 	}
 
-	private String getCalendarString() {
+	private String getCalendarString(Date datum) {
 		URL oracle;
 		BufferedReader in;
 		String inputLine = "";
 		String response = "";
 		try {
-			oracle = new URL("http://api.dryg.net/dagar/v1/");
+			String datumtext = new SimpleDateFormat("yyyyMMdd").format(datum);
+			String urlString = "http://api.dryg.net/dagar/v1/?datum="
+					+ datumtext;
+			oracle = new URL(urlString);
 			in = new BufferedReader(new InputStreamReader(oracle.openStream()));
 			while ((inputLine = in.readLine()) != null) {
 				response += inputLine;
+				// System.out.println("input " + datum.toLocaleString() +
+				// " output " + inputLine);
 			}
 
 			in.close();
