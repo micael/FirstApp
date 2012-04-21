5ccdba23c2a6de526a749df8b9a05e0a93529876
Micael Vesterlund <micael.vesterlund@gmail.com>
2012-04-21 23:20:41 +0200

    new font for red day

--- FirstApp/src/se/janochmicael/firstapp/FirstAppActivity.java
+++ FirstApp/src/se/janochmicael/firstapp/FirstAppActivity.java
@@ -23,6 +23,7 @@ public class FirstAppActivity extends Activity {
 	Button submitButton;
 	DatePicker datePicker;
 	TextView calendarTV;
+	TextView redDayTV;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -36,8 +37,7 @@ public class FirstAppActivity extends Activity {
 
 		submitButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
-				Date d = new Date(datePicker.getYear() - 1900, datePicker
-						.getMonth(), datePicker.getDayOfMonth());
+				Date d = new Date(datePicker.getYear() - 1900, datePicker.getMonth(), datePicker.getDayOfMonth());
 				updateText(getCalendarString(d));
 			}
 
@@ -45,6 +45,7 @@ public class FirstAppActivity extends Activity {
 		// submitButton.on
 
 		calendarTV = (TextView) findViewById(R.id.calendar);
+		redDayTV = (TextView) findViewById(R.id.redDay);
 
 		String response = getCalendarString(Calendar.getInstance().getTime());
 		updateText(response);
@@ -54,43 +55,40 @@ public class FirstAppActivity extends Activity {
 	private void updateText(String response) {
 		try {
 			JSONObject jsonObject = new JSONObject(response);
-			StringBuffer calendarText = parseAndFormat(jsonObject);
+			StringBuffer calendarText = parseAndFormatCalendar(jsonObject);
 			calendarTV.setText(calendarText);
+			StringBuffer redDayText = parseAndFormatRedDay(jsonObject);
+			redDayTV.setText(redDayText);
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// nada
 		}
 	}
 
-	private StringBuffer parseAndFormat(JSONObject jsonObject)
-			throws JSONException {
+	private StringBuffer parseAndFormatRedDay(JSONObject jsonObject) {
 		StringBuffer calendarText = new StringBuffer();
-		String datum = jsonObject.getString("datum");
-		String unixdatum = jsonObject.getString("unixdatum");
-		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-		Date date = new Date(Long.valueOf(unixdatum));
-		format.format(date);
-		String dag = jsonObject.getString("dag");
-		// String veckodag = jsonObject.getString("veckodag");
-		String vecka = jsonObject.getString("vecka");
-		calendarText.append(datum);
-		calendarText.append(" ");
-		calendarText.append(format.format(date));
-		calendarText.append(" ");
-		calendarText.append(dag);
-		calendarText.append(" ");
-		// calendarText.append(veckodag);
-		calendarText.append(" V:");
-		calendarText.append(vecka);
 		try {
 			String helgdag = jsonObject.getString("helgdag");
 			if (helgdag != null && !"".equals(helgdag)) {
-				calendarText.append(" ").append(helgdag);
+				calendarText.append(helgdag);
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			// nada
 		}
 
+		return calendarText;
+	}
+
+	private StringBuffer parseAndFormatCalendar(JSONObject jsonObject) throws JSONException {
+		StringBuffer calendarText = new StringBuffer();
+		String datum = jsonObject.getString("datum");
+		String dag = jsonObject.getString("dag");
+		String vecka = jsonObject.getString("vecka");
+		calendarText.append(datum);
+		calendarText.append(" ");
+		calendarText.append(dag);
+		calendarText.append(" ");
+		calendarText.append(" V:");
+		calendarText.append(vecka);
 		return calendarText;
 	}
 
@@ -101,8 +99,7 @@ public class FirstAppActivity extends Activity {
 		String response = "";
 		try {
 			String datumtext = new SimpleDateFormat("yyyyMMdd").format(datum);
-			String urlString = "http://api.dryg.net/dagar/v1/?datum="
-					+ datumtext;
+			String urlString = "http://api.dryg.net/dagar/v1/?datum=" + datumtext;
 			oracle = new URL(urlString);
 			in = new BufferedReader(new InputStreamReader(oracle.openStream()));
 			while ((inputLine = in.readLine()) != null) {
